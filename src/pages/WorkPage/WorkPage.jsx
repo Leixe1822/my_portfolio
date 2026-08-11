f58bc6f60f114536530be88decb0c89dd37754d7
Leixe1822 <worklexie22@gmail.com>
2026-08-11 22:26:06 +0800

    Update projects page cards

--- src/pages/WorkPage/WorkPage.jsx
+++ src/pages/WorkPage/WorkPage.jsx
@@ -1,4 +1,4 @@
-/* WorkPage 頁面：作品集列表與案例研究入口 */
+/* WorkPage 頁面：呈現作品標題與 Projects 卡片列表 */
 import Projects from '../../sections/Projects/Projects'
 import './WorkPage.css'
 
@@ -8,6 +8,12 @@ function WorkPage() {
       <section className="work-page__hero">
         <p>WORK / CASE STUDIES</p>
         <h1>Projects</h1>
+
+        {/* 作品頁小裝飾：輕量標記，不搶下方作品卡重點 */}
+        <div className="work-page__mini-mark" aria-hidden="true">
+          <span />
+          03 works
+        </div>
       </section>
 
       <Projects />
